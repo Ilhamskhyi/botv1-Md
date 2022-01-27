b189d187061856f2230649e1bcb94cb8f8c7ef49
Ilhamskhyi <98175032+Ilhamskhyi@users.noreply.github.com>
2022-01-28 00:51:24 +0700

    Add files via upload

---
+++ plugins/sourcecode.js
@@ -0,0 +1,50 @@
+/**
+* jangan ganti ya kakak kakak sekalian
+* ini cuma buat ninggalin credit gw doang :)
+**/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
+let handler = async (m) => {
+let esce = `
+Bot ini Menggunakan Script Dari
+
+https://github.com/Ilhamskhzyi/botv1-Md`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: esce,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/esce.jpg') },           
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '𝙶𝙴𝚃 𝙱𝙾𝚃',
+               url: 'https://github.com/Ilhamskhzyi/botv1-Md'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: '𝙱𝙰𝙲𝙺 𝚃𝙾 𝙼𝙴𝙽𝚄',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['sc', 'sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc|sourcecode)$/i
+
+module.exports = handler
+
+
---
+++ plugins/tqto.js
@@ -0,0 +1,64 @@
+/**
+* jangan dihapus/diganti ya kontol
+* lu itu cuma make jadi jangan diapa apain ya bangsat
+* mending lu tambahin deh nama lu jangan hapus kreditnya
+**/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
+let moment = require('moment-timezone')
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let tqto = `
+*BIG THANKS TO*
+
+Nurutomo: 
+https://github.com/Nurutomo
+Istikmal: 
+https://github.com/BochilGaming
+Ariffb: 
+https://github.com/Ariffb25
+ilham: 
+https://github.com/Ilhamskhzyi
+
+`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: tqto,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/tqto.jpg') }, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '𝙶𝙴𝚃 𝙱𝙾𝚃',
+               url: 'https://github.com/Ilhamskhzyi/botv1-Md'
+             }
+
+           },
+       
+               {
+             quickReplyButton: {
+               displayText: '𝙱𝙰𝙲𝙺 𝚃𝙾 𝙼𝙴𝙽𝚄',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['tqto']
+handler.tags = ['info']
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+
+module.exports = handler
